26d2c2e5f3fd9134f620e45f6b31c6f7ecb65a97
herjatiaji <herjati.aji@mail.ugm.ac.id>
2023-05-03 14:48:01 +0700

    commit suicide

--- app/src/main/java/com/example/canvas/MainActivity.java
+++ app/src/main/java/com/example/canvas/MainActivity.java
@@ -6,10 +6,15 @@ import androidx.core.content.res.ResourcesCompat;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.Point;
+
 import android.graphics.Rect;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+
+
 
 public class MainActivity extends AppCompatActivity {
     private Canvas mCanvas;
@@ -20,7 +25,8 @@ public class MainActivity extends AppCompatActivity {
     private Rect mRect = new Rect();
     private Rect mBounds = new Rect();
 
-    private static final int OFFSET = 120;
+
+    private static final int OFFSET = 12;
     private int mOffset = OFFSET;
     private static final  int MULTIPLIER = 100;
 
@@ -72,11 +78,40 @@ public class MainActivity extends AppCompatActivity {
             mOffset += OFFSET;
         }
         else {
-            mPaint.setColor(mColorRectangle-MULTIPLIER*mOffset);
-            mRect.set(mOffset,mOffset,vWidth-mOffset,
-                    vHeight - mOffset);
-            mCanvas.drawRect(mRect, mPaint);
-            mOffset += OFFSET;
+            if (mOffset < halfWidth && mOffset < halHeight){
+                mPaint.setColor(mColorRectangle-MULTIPLIER*mOffset);
+                mRect.set(mOffset,mOffset,vWidth-mOffset,
+                        vHeight - mOffset);
+                mCanvas.drawRect(mRect, mPaint);
+                mOffset += OFFSET;
+            }
+            else{
+                mPaint.setColor(mColorCircle - MULTIPLIER * mOffset);
+                mCanvas.drawCircle(halfWidth,halHeight,halfWidth/3,mPaint);
+                String text = getString(R.string.done);
+                mPaintText.getTextBounds(text,0,text.length(),mBounds);
+                int x = halfWidth - mBounds.centerX();
+                int y = halHeight - mBounds.centerY();
+                mCanvas.drawText(text, x,y, mPaintText);
+                mOffset += OFFSET;
+
+                Point a = new Point(halfWidth-50,halHeight-50);
+                Point b = new Point(halfWidth-50,halHeight-50);
+                Point c = new Point(halfWidth-50,halHeight-250);
+
+                Path path = new Path();
+                path.setFillType(Path.FillType.EVEN_ODD);
+                path.lineTo(a.x,a.y);
+                path.lineTo(b.x,b.y);
+                path.lineTo(c.x,c.y);
+                path.lineTo(a.x,a.y);
+                path.close();
+
+                mCanvas.drawPath(path,mPaint);
+                mOffset += OFFSET;
+
+            }
+
         }
 
         v.invalidate();
