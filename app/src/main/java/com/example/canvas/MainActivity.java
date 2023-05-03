ebe6167b6ca1044adcaed6d3f1d8f54c6e5b88be
herjatiaji <herjati.aji@mail.ugm.ac.id>
2023-05-03 13:47:45 +0700

    second commit

--- app/src/main/java/com/example/canvas/MainActivity.java
+++ app/src/main/java/com/example/canvas/MainActivity.java
@@ -1,12 +1,14 @@
 package com.example.canvas;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
 
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Rect;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
@@ -22,13 +24,49 @@ public class MainActivity extends AppCompatActivity {
     private int mOffset = OFFSET;
     private static final  int MULTIPLIER = 100;
 
+    private int mColorText;
     private int mColorBackground;
     private int mColorRectangle;
-    private int mColorAccent;
+    private int mColorCircle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mColorBackground = ResourcesCompat.getColor(getResources(), R.color.colorBackground, null);
+        mColorRectangle = ResourcesCompat.getColor(getResources(),R.color.colorRectangle,null);
+        mColorCircle = ResourcesCompat.getColor(getResources(),R.color.colorAccent,null);
+        mColorText = ResourcesCompat.getColor(getResources(),R.color.black,null);
+
+        mPaint.setColor(mColorBackground);
+        mPaintText.setColor(mColorText);
+        mPaintText.setTextSize(70);
+
+        mImageView = findViewById(R.id.my_image_view);
+
+        mImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawSomething(v);
+            }
+        });
+
+
+    }
+
+    private void drawSomething(View v) {
+        int vWidth = v.getWidth();
+        int vHeight = v.getHeight();
+
+        int halfWidth = vWidth/2;
+        int halHeight = vHeight/2;
+
+        if(mOffset == OFFSET){
+            mBitmap = Bitmap.createBitmap(vWidth,vHeight,Bitmap.Config.ARGB_8888);
+            mImageView.setImageBitmap(mBitmap);
+            mCanvas = new Canvas(mBitmap);
+            mCanvas.drawColor(mColorBackground);
+        }
     }
 }
