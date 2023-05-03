5f4b2ee27b22e884d2ed76c2dbceb1af6f9b3cb6
herjatiaji <herjati.aji@mail.ugm.ac.id>
2023-05-03 13:34:56 +0700

    Initial commit

---
+++ app/src/main/java/com/example/canvas/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.canvas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    private Canvas mCanvas;
+    private Paint mPaint = new Paint();
+    private Paint mPaintText = new Paint(Paint.UNDERLINE_TEXT_FLAG);
+    private Bitmap mBitmap;
+    private ImageView mImageView;
+    private Rect mRect = new Rect();
+    private Rect mBounds = new Rect();
+
+    private static final int OFFSET = 120;
+    private int mOffset = OFFSET;
+    private static final  int MULTIPLIER = 100;
+
+    private int mColorBackground;
+    private int mColorRectangle;
+    private int mColorAccent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
